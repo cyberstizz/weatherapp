98717124dba9f8ffa556a6c9b8a4893d00906ff4
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-18 23:26:16 -0400

    added react rouuter to the party

---
+++ src/components/monday.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+export default class Monday extends React.Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            data: []
+        }
+    }
+    
+    
+    render(){
+  return (
+    <div id="mondaySolo" >
+
+    </div>
+  );
+}
+
+}
