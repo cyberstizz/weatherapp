47dd656637f65b5957c6374fca7bc2ecbd1d35e8
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-18 22:42:26 -0400

    added spaces for each day on main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div id="daysDiv">
+      <div id="mondayDiv"></div>
+      <div id="tuesdayDiv"></div>
+      <div id="wednesdayDiv"></div>
+      <div id="thursdayDiv"></div>
+      <div id="fridayDiv"></div>
+      <div id="saturdayDiv"></div>
+      <div id="sundayDiv"></div>
+
+     </div>
     </div>
   );
 }
