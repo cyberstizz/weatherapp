e99f3a4be64d5ca7b6d92cb1325f7a9705e17b8a
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-19 16:23:32 -0400

    added some dividers to the setions

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ function App() {
   return (
     <div className="App">
      <div id="daysDiv">
-      <div id="mondayDiv"></div>
-      <div id="tuesdayDiv"></div>
-      <div id="wednesdayDiv"></div>
-      <div id="thursdayDiv"></div>
-      <div id="fridayDiv"></div>
-      <div id="saturdayDiv"></div>
-      <div id="sundayDiv"></div>
+      <div id="mondayDiv">
+        <div id="topRow"></div><div id="imageRow"></div><div id="imageRow"></div>
+      </div>
+      <div id="tuesdayDiv">
+      <div id="topRow"></div><div id="imageRow"></div><div id="imageRow"></div>
+      </div>
+      <div id="wednesdayDiv">
+      <div id="topRow"></div><div id="imageRow"></div><div id="imageRow"></div>
+      </div>
+      <div id="thursdayDiv">
+      <div id="topRow"></div><div id="imageRow"></div><div id="imageRow"></div>
+      </div>
+      <div id="fridayDiv">
+      <div></div><div></div><div></div>
+      </div>
+      <div id="saturdayDiv">
+      <div></div><div></div><div></div>
+      </div>
+      <div id="sundayDiv">
+      <div></div><div></div><div></div>
+      </div>
 
      </div>
     </div>
